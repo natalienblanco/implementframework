cb4b9ea1c7a15393aa5b6c3745bda9e238d58d7d
Natalie Blanco <44891833+natalienblanco@users.noreply.github.com>
2019-04-29 09:55:45 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Create simple website using codeigniter</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<!-- Latest compiled and minified Jquery library -->
+<script src='https://code.jquery.com/jquery-2.1.1.min.js'></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row clearfix">
+<div class="col-md-12 column">
+<nav class="navbar navbar-default" role="navigation">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">CodeIgniter</a>
+</div>
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<ul class="nav navbar-nav">
+<li class="active">
+<a href="#">Home</a>
+</li>
+<li>
+<a href="#">Portfolio</a>
+</li>
+<li>
+<a href="#">About Me</a>
+</li>
+<li>
+<a href="#">Contact Me</a>
+</li>
+</ul>
+</div>
+</nav>
+</div>
+</div>
+<blockquote>
+<h1>Example of a simple webpage using Codeigniter</h1>
+<cite>By Natalie Blanco</cite>
+</blockquote>
+</div>
+</body>
+</html>
